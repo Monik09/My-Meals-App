37a7385f970326b1c90aede03b9e939443759c12
Agus Himawan <mr.agus.him@gitlab.com>
2020-10-17 20:10:48 +0700

    Added new line on Dictionary

--- lib/shared/dictionary.dart
+++ lib/shared/dictionary.dart
@@ -1,5 +1,5 @@
 const appname= 'My Meals App';
-const addFavoritesMsg= "Start adding some favorites ";
-const yourFilter="Your Filters";
-const yourFavorites="Your Favorites";
+const addFavoritesMsg= 'Start adding some favorites ';
+const yourFilter='Your Filters';
+const yourFavorites='Your Favorites';
 const categories ='Categories';
